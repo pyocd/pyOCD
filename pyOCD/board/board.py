pyocd/pyOCD

commit f55ecc9bf9d9cb3fd380ab29774c1618a4f18eda

Fix a different python hang (on Windows) when an error occurred during the flash.init().
* No matter what, you have to call interface.close() to insure python.exe on Windows won't hang.

Signed-off-by: Chris Williamson <chris.williamson@freescale.com>

pyOCD/board/board.py
@@ -53,9 +53,17 @@ class Board(object):
         This function resumes the target
         """
         logging.debug("uninit board %s", self)
-        self.target.resume()
-        self.transport.uninit()
-        self.interface.close()
+        try:
+            self.target.resume()
+            self.transport.uninit()
+        except Exception as e:
+            try:
+                print e
+            except:
+                pass
+            finally:
+                self.interface.close()
+                raise e
     
     def getInfo(self):
         return self.interface.getInfo()

commit 9a1434434c87952a0f5296bb7995e038be3868af

Close interface to prevent hanging on windows
In board.py the function uninit only closes the interface if an
exception occurs in either target.resume() or transport.uninit().
This patch moves the finally block running interface.close to
the top try block so the interface always gets closed.
This fixes the hanging commonly seen on windows.

This problem was introduced in the commit
f55ecc9bf9d9cb3fd380ab29774c1618a4f18eda - "Fix a different python
hang (on Windows) when an error occurred during the flash.init()."

pyOCD/board/board.py
@@ -56,14 +56,8 @@ class Board(object):
         try:
             self.target.resume()
             self.transport.uninit()
-        except Exception as e:
-            try:
-                print e
-            except:
-                pass
-            finally:
-                self.interface.close()
-                raise e
+        finally:
+            self.interface.close()
     
     def getInfo(self):
         return self.interface.getInfo()