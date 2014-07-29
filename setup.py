28ea55dbba2343c77883e7572c4b84a0e3e09d00
sg- <sam.w.grove@gmail.com>
2014-07-29 09:57:54 -0500

    Update usage of PyOCD to pyOCD

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from distutils.core import setup
 setup(
     name="pyOCD",
     version="0.2",
-    description="CMSIS-DAP debugger for python",
+    description="CMSIS-DAP debugger for Python",
     author="samux, emilmont",
     author_email="Samuel.Mokrani@arm.com, Emilio.Monti@arm.com",
     license="Apache 2.0",
