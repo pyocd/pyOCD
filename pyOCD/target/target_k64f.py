1113c12e71a45eb7bdac51181d62c990a0eb952e
Brendan Moran <bremor01@e105944-mac.emea.arm.com>
2014-11-12 15:31:26 +0000

    Adjust K64F target file memory configuration to match K64F datasheet.

--- pyOCD/target/target_k64f.py
+++ pyOCD/target/target_k64f.py
@@ -25,7 +25,7 @@ class K64F(Kinetis):
 <!DOCTYPE memory-map PUBLIC "+//IDN gnu.org//DTD GDB Memory Map V1.0//EN" "http://sourceware.org/gdb/gdb-memory-map.dtd">
 <memory-map>
     <memory type="flash" start="0x0" length="0x100000"> <property name="blocksize">0x1000</property></memory>
-    <memory type="ram" start="0x1ffe0000" length="0x40000"> </memory>
+    <memory type="ram" start="0x1fff0000" length="0x40000"> </memory>
 </memory-map>
 """
     
