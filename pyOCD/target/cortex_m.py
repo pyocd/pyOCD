9ce0a78ae3414744af8c88227ecc3827de0cdb77
Chris Reed <creed@freescale.com>
2015-06-11 09:33:05 -0500

    Commented out unaligned mem read/write ragged edge debug logs in CortexM.
    Signed-off-by: Chris Reed <creed@freescale.com>

--- pyOCD/target/cortex_m.py
+++ pyOCD/target/cortex_m.py
@@ -543,7 +543,7 @@ class CortexM(Target):
         # try to read 8bits data
         if (size > 0) and (addr & 0x01):
             mem = self.readMemory(addr, 8)
-            logging.debug("get 1 byte at %s: 0x%X", hex(addr), mem)
+#             logging.debug("get 1 byte at %s: 0x%X", hex(addr), mem)
             res.append(mem)
             size -= 1
             addr += 1
@@ -551,7 +551,7 @@ class CortexM(Target):
         # try to read 16bits data
         if (size > 1) and (addr & 0x02):
             mem = self.readMemory(addr, 16)
-            logging.debug("get 2 bytes at %s: 0x%X", hex(addr), mem)
+#             logging.debug("get 2 bytes at %s: 0x%X", hex(addr), mem)
             res.append(mem & 0xff)
             res.append((mem >> 8) & 0xff)
             size -= 2
@@ -567,7 +567,7 @@ class CortexM(Target):
 
         if (size > 1):
             mem = self.readMemory(addr, 16)
-            logging.debug("get 2 bytes at %s: 0x%X", hex(addr), mem)
+#             logging.debug("get 2 bytes at %s: 0x%X", hex(addr), mem)
             res.append(mem & 0xff)
             res.append((mem >> 8) & 0xff)
             size -= 2
@@ -575,7 +575,7 @@ class CortexM(Target):
 
         if (size > 0):
             mem = self.readMemory(addr, 8)
-            logging.debug("get 1 byte remaining at %s: 0x%X", hex(addr), mem)
+#             logging.debug("get 1 byte remaining at %s: 0x%X", hex(addr), mem)
             res.append(mem)
             size -= 1
             addr += 1
@@ -592,7 +592,7 @@ class CortexM(Target):
 
         #try to write 8 bits data
         if (size > 0) and (addr & 0x01):
-            logging.debug("write 1 byte at 0x%X: 0x%X", addr, data[idx])
+#             logging.debug("write 1 byte at 0x%X: 0x%X", addr, data[idx])
             self.writeMemory(addr, data[idx], 8)
             size -= 1
             addr += 1
@@ -600,7 +600,7 @@ class CortexM(Target):
 
         # try to write 16 bits data
         if (size > 1) and (addr & 0x02):
-            logging.debug("write 2 bytes at 0x%X: 0x%X", addr, data[idx] | (data[idx+1] << 8))
+#             logging.debug("write 2 bytes at 0x%X: 0x%X", addr, data[idx] | (data[idx+1] << 8))
             self.writeMemory(addr, data[idx] | (data[idx+1] << 8), 16)
             size -= 2
             addr += 2
@@ -617,7 +617,7 @@ class CortexM(Target):
 
         # try to write 16 bits data
         if (size > 1):
-            logging.debug("write 2 bytes at 0x%X: 0x%X", addr, data[idx] | (data[idx+1] << 8))
+#             logging.debug("write 2 bytes at 0x%X: 0x%X", addr, data[idx] | (data[idx+1] << 8))
             self.writeMemory(addr, data[idx] | (data[idx+1] << 8), 16)
             size -= 2
             addr += 2
@@ -625,7 +625,7 @@ class CortexM(Target):
 
         #try to write 8 bits data
         if (size > 0):
-            logging.debug("write 1 byte at 0x%X: 0x%X", addr, data[idx])
+#             logging.debug("write 1 byte at 0x%X: 0x%X", addr, data[idx])
             self.writeMemory(addr, data[idx], 8)
             size -= 1
             addr += 1
