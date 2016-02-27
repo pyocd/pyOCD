dcbf5d0974b5f05a959d0c0573b2bd61b0db6d9b
Chris Reed <flit@me.com>
2016-02-27 14:44:41 -0600

    Fix CMSIS_DAP_Protocol.dapInfo().
    - It was not accepting an integer info ID, but that's what was being
      passed in from DAPAccessUSB.
    - Stripping off a terminating null char on string values returned from
      the target.

--- pyOCD/pyDAPAccess/cmsis_dap_core.py
+++ pyOCD/pyDAPAccess/cmsis_dap_core.py
@@ -18,6 +18,7 @@
 import logging
 import array
 from .dap_access_api import DAPAccessIntf
+import six
 
 COMMAND_ID = {'DAP_INFO': 0x00,
               'DAP_LED': 0x01,
@@ -82,7 +83,9 @@ class CMSIS_DAP_Protocol(object):
     def dapInfo(self, id_):
         cmd = []
         cmd.append(COMMAND_ID['DAP_INFO'])
-        cmd.append(ID_INFO[id_])
+        if not type(id_) in (six.integer_types):
+            id_ = ID_INFO[id_]
+        cmd.append(id_)
         self.interface.write(cmd)
 
         resp = self.interface.read()
@@ -100,10 +103,12 @@ class CMSIS_DAP_Protocol(object):
             if resp[1] == 2:
                 return (resp[3] << 8) | resp[2]
 
-        # String values
-        x = array.array('B', [i for i in resp[2:2 + resp[1]]])
+        # String values. They are sent as C strings with a terminating null char, so we strip it out.
+        x = array.array('B', [i for i in resp[2:2 + resp[1]]]).tostring()
+        if x[-1] == '\x00':
+            x = x[0:-1]
+        return x
 
-        return x.tostring()
 
     def setLed(self):
         #not yet implemented
