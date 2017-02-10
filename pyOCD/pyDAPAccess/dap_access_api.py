4aee0657f0831a7d67b9765ed554323786e725be
Chris Reed <flit@me.com>
2017-02-10 16:04:11 -0600

    TransferFaultError holds the faulting address.
    - Added fault_address property to TransferFaultError.
    - MEM_AP read/write methods set the fault address and reraise if they
      see a TransferFaultError.

--- pyOCD/pyDAPAccess/dap_access_api.py
+++ pyOCD/pyDAPAccess/dap_access_api.py
@@ -73,7 +73,19 @@ class DAPAccessIntf(object):
 
     class TransferFaultError(TransferError):
         """A SWD Fault occurred"""
-        pass
+        def __init__(self, faultAddress=None):
+            super(DAPAccessIntf.TransferFaultError, self).__init__(faultAddress)
+            self._address = faultAddress
+
+        @property
+        def fault_address(self):
+            return self._address
+
+        def __str__(self):
+            desc = "SWD/JTAG Transfer Fault"
+            if self._address is not None:
+                desc += " @ 0x%08x" % self._address
+            return desc
 
     class TransferProtocolError(TransferError):
         """A SWD protocol error occurred"""
