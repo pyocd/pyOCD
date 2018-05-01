fdb5b28a9c7e19134d6544a4adbcea0dd8d49d25
Lin Gao <lin.gao@arm.com>
2018-05-01 14:26:05 -0500

    Changed blocksize of KW41Z to 0x800. Per datasheet, block erase is on 2K granularity.

--- pyOCD/target/target_MKW41Z512xxx4.py
+++ pyOCD/target/target_MKW41Z512xxx4.py
@@ -88,7 +88,7 @@ class Flash_kw41z4(Flash_Kinetis):
 class KW41Z4(Kinetis):
 
     memoryMap = MemoryMap(
-        FlashRegion(    start=0,           length=0x28000,      blocksize=0x400, isBootMemory=True),
+        FlashRegion(    start=0,           length=0x28000,      blocksize=0x800, isBootMemory=True),
         RamRegion(      start=0x1ffff000,  length=0x5000)
         )
 
