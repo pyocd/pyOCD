28065c2e262cfab3759a541da2bcf14ab45fe446
Chris Reed <flit@me.com>
2018-07-25 17:58:11 -0500

    Added makefiles for analyzer and gdb_test_program.
    - This lets users build these projects from macOS or Linux. The .bat
      build scripts are still present for Windows.
    - Modified analyzer generate_python.py to be Python3 compatible, as well
      as take input and output files as arguments.
    - Rebuilt analyzer and gdb_test_program with gcc 7.3.1 (GNU Tools for
      Arm Embedded Processors 7-2018-q2-update).
    - Updated .gitignore files to ignore .o, .d, and .map, and added a
      local .gitignore in analyzer/ to ignore the main.{bin,elf,py} outputs.

--- src/analyzer/build.bat
+++ src/analyzer/build.bat
@@ -1,4 +1,3 @@
-set path=%path%;C:\Program Files (x86)\GNU Tools ARM Embedded\4.9 2014q4\bin
 arm-none-eabi-gcc.exe -O3 -ffunction-sections -fdata-sections -Wall -mcpu=cortex-m0 -mthumb -mfloat-abi=soft -g3 -gdwarf-2 -gstrict-dwarf -T"linker_script.ld" -Wl,-Map,main.map,--gc-sections,-ecompute_crc -n -mcpu=cortex-m0 -mthumb -mfloat-abi=soft -g3 -nostdlib -fpic -ffixed-r9 main.c -o main.elf
 arm-none-eabi-objcopy.exe --output-target binary main.elf main.bin
-python.exe generate_python.py
+python.exe generate_python.py main.bin main.py
