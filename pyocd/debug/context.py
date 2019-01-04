4ec1f36580aa372ec6ae15575d1be87347acb508
Chris Reed <flit@me.com>
2019-01-04 15:47:24 -0600

    Fixed DebugContext not converting 64-bit floats.

--- pyocd/debug/context.py
+++ pyocd/debug/context.py
@@ -1,6 +1,6 @@
 """
  mbed CMSIS-DAP debugger
- Copyright (c) 2016 ARM Limited
+ Copyright (c) 2016-2019 ARM Limited
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -16,7 +16,8 @@
 """
 
 from ..core.memory_interface import MemoryInterface
-from ..coresight.cortex_m import (CORE_REGISTER, register_name_to_index, is_float_register)
+from ..coresight.cortex_m import (CORE_REGISTER, register_name_to_index, is_float_register,
+                                    is_double_float_register)
 from ..utility import conversion
 import logging
 
@@ -65,6 +66,8 @@ class DebugContext(MemoryInterface):
         # Convert int to float.
         if is_float_register(regIndex):
             regValue = conversion.u32_to_float32(regValue)
+        elif is_double_float_register(regIndex):
+            regValue = conversion.u64_to_float64(regValue)
         return regValue
 
     def read_core_register_raw(self, reg):
@@ -88,6 +91,8 @@ class DebugContext(MemoryInterface):
         # Convert float to int.
         if is_float_register(regIndex) and type(data) is float:
             data = conversion.float32_to_u32(data)
+        elif is_double_float_register(regIndex) and type(data) is float:
+            data = conversion.float64_to_u64(data)
         self.write_core_register_raw(regIndex, data)
 
     def write_core_register_raw(self, reg, data):
