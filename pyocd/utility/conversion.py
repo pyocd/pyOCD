30fc53b506c43fed720e35e14d44ff50ece006d0
Chris Reed <flit@me.com>
2019-02-01 16:29:53 -0600

    Padding support for byte_list_to_u32le_list() and align_up() utility.
    - byte_list_to_u32le_list() will pad unaligned lists of data with a
      configurable value.
    - Added align_up() utility.
    - Made some doc comments doxygen-compatible.
    - Added unit tests.

--- pyocd/utility/conversion.py
+++ pyocd/utility/conversion.py
@@ -1,32 +1,39 @@
-"""
- mbed CMSIS-DAP debugger
- Copyright (c) 2015 ARM Limited
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-"""
+# pyOCD debugger
+# Copyright (c) 2015-2019 Arm Limited
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import struct
 import binascii
 import six
 
-def byte_list_to_u32le_list(data):
-    """Convert a list of bytes to a list of 32-bit integers (little endian)"""
+def byte_list_to_u32le_list(data, pad=0x00):
+    """! @brief Convert a list of bytes to a list of 32-bit integers (little endian)
+    
+    If the length of the data list is not a multiple of 4, then the pad value is used
+    for the additional required bytes.
+    """
     res = []
     for i in range(len(data) // 4):
         res.append(data[i * 4 + 0] |
                    data[i * 4 + 1] << 8 |
                    data[i * 4 + 2] << 16 |
                    data[i * 4 + 3] << 24)
+    remainder = (len(data) % 4)
+    if remainder != 0:
+        padCount = 4 - remainder
+        res += byte_list_to_u32le_list(list(data[-remainder:]) + [pad] * padCount)
     return res
 
 
