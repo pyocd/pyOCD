529e3ec4ef07dd3965296d18b4993fefdf5428ff
Chris Reed <flit@me.com>
2019-02-07 13:41:37 -0600

    Cleanup docs for conversion and mask utilities.

--- pyocd/utility/conversion.py
+++ pyocd/utility/conversion.py
@@ -36,9 +36,8 @@ def byte_list_to_u32le_list(data, pad=0x00):
         res += byte_list_to_u32le_list(list(data[-remainder:]) + [pad] * padCount)
     return res
 
-
 def u32le_list_to_byte_list(data):
-    """Convert a word array into a byte array"""
+    """! @brief Convert a word array into a byte array"""
     res = []
     for x in data:
         res.append((x >> 0) & 0xff)
@@ -47,48 +46,42 @@ def u32le_list_to_byte_list(data):
         res.append((x >> 24) & 0xff)
     return res
 
-
 def u16le_list_to_byte_list(data):
-    """Convert a halfword array into a byte array"""
+    """! @brief Convert a halfword array into a byte array"""
     byteData = []
     for h in data:
         byteData.extend([h & 0xff, (h >> 8) & 0xff])
     return byteData
 
-
 def byte_list_to_u16le_list(byteData):
-    """Convert a byte array into a halfword array"""
+    """! @brief Convert a byte array into a halfword array"""
     data = []
     for i in range(0, len(byteData), 2):
         data.append(byteData[i] | (byteData[i + 1] << 8))
     return data
 
-
 def u32_to_float32(data):
-    """Convert a 32-bit int to an IEEE754 float"""
+    """! @brief Convert a 32-bit int to an IEEE754 float"""
     d = struct.pack(">I", data)
     return struct.unpack(">f", d)[0]
 
-
 def float32_to_u32(data):
-    """Convert an IEEE754 float to a 32-bit int"""
+    """! @brief Convert an IEEE754 float to a 32-bit int"""
     d = struct.pack(">f", data)
     return struct.unpack(">I", d)[0]
 
-
 def u64_to_float64(data):
-    """Convert a 64-bit int to an IEEE754 float"""
+    """! @brief Convert a 64-bit int to an IEEE754 float"""
     d = struct.pack(">Q", data)
     return struct.unpack(">d", d)[0]
 
-
 def float64_to_u64(data):
-    """Convert an IEEE754 float to a 64-bit int"""
+    """! @brief Convert an IEEE754 float to a 64-bit int"""
     d = struct.pack(">d", data)
     return struct.unpack(">Q", d)[0]
 
 def u32_to_hex8le(val):
-    """Create 8-digit hexadecimal string from 32-bit register value"""
+    """! @brief Create 8-digit hexadecimal string from 32-bit register value"""
     return ''.join("%02x" % (x & 0xFF) for x in (
         val,
         val >> 8,
@@ -96,9 +89,8 @@ def u32_to_hex8le(val):
         val >> 24,
     ))
 
-
 def u64_to_hex16le(val):
-    """Create 16-digit hexadecimal string from 64-bit register value"""
+    """! @brief Create 16-digit hexadecimal string from 64-bit register value"""
     return ''.join("%02x" % (x & 0xFF) for x in (
         val,
         val >> 8,
@@ -111,34 +103,33 @@ def u64_to_hex16le(val):
     ))
 
 def hex8_to_u32be(data):
-    """Build 32-bit register value from big-endian 8-digit hexadecimal string"""
+    """! @brief Build 32-bit register value from big-endian 8-digit hexadecimal string"""
     return int(data[6:8] + data[4:6] + data[2:4] + data[0:2], 16)
 
 def hex16_to_u64be(data):
-    """Build 64-bit register value from big-endian 16-digit hexadecimal string"""
+    """! @brief Build 64-bit register value from big-endian 16-digit hexadecimal string"""
     return int(data[14:16] + data[12:14] + data[10:12] + data[8:10] + data[6:8] + data[4:6] + data[2:4] + data[0:2], 16)
 
 def hex8_to_u32le(data):
-    """Build 32-bit register value from little-endian 8-digit hexadecimal string"""
+    """! @brief Build 32-bit register value from little-endian 8-digit hexadecimal string"""
     return int(data[0:8], 16)
 
 def hex16_to_u64le(data):
-    """Build 64-bit register value from little-endian 8-digit hexadecimal string"""
+    """! @brief Build 64-bit register value from little-endian 16-digit hexadecimal string"""
     return int(data[0:16], 16)
 
 def byte_to_hex2(val):
-    """Create 2-digit hexadecimal string from 8-bit value"""
+    """! @brief Create 2-digit hexadecimal string from 8-bit value"""
     return "%02x" % int(val)
 
-
 def hex_to_byte_list(data):
-    """Convert string of hex bytes to list of integers"""
+    """! @brief Convert string of hex bytes to list of integers"""
     return list(six.iterbytes(binascii.unhexlify(data)))
 
-
 def hex_decode(cmd):
+    """! @brief Return the binary data represented by the hexadecimal string."""
     return binascii.unhexlify(cmd)
 
-
 def hex_encode(string):
+    """! @brief Return the hexadecimal representation of the binary data."""
     return binascii.hexlify(string)
