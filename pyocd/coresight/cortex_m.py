280e628e433df9559b4939b2368f51d910b26109
Chris Reed <flit@me.com>
2019-02-26 17:30:05 -0600

    is_float_register() return True for either single or double precision floats.
    - Removed warning log message.

--- pyocd/coresight/cortex_m.py
+++ pyocd/coresight/cortex_m.py
@@ -162,8 +162,7 @@ def register_name_to_index(reg):
     return reg
 
 def is_float_register(index):
-    logging.warning("cortex_m.is_float_register() is deprecated - use is_single_float_register() and/or consider other functions")
-    return is_single_float_register(index)
+    return is_single_float_register(index) or is_double_float_register(index)
 
 # Returns true for registers holding single-precision float values
 def is_single_float_register(index):
