43cf02f11a7c5a111221344330c52c60e21fb47d
Chris Reed <flit@me.com>
2019-06-03 18:18:28 -0500

    Added test binary for LPCXpresso55S69.

--- pyocd/board/board_ids.py
+++ pyocd/board/board_ids.py
@@ -54,7 +54,7 @@ BOARD_ID_TO_INFO = {
     "0233": BoardInfo(  "FRDM-KE16Z",           "mke16z64vlf4",     None,                   ),
     "0234": BoardInfo(  "Rapid-IoT-KW41Z",      "kw41z4",           "l1_kw41z4.bin",        ),
     "0235": BoardInfo(  "LPC54018IoTModule",    "lpc54018jet180",   None,                   ),
-    "0236": BoardInfo(  "LPCXpresso55S69",      "lpc55s69",         None,                   ),
+    "0236": BoardInfo(  "LPCXpresso55S69",      "lpc55s69",         "lpcxpresso55s69.bin",  ),
     "0240": BoardInfo(  "FRDM-K64F",            "k64f",             "l1_k64f.bin",          ),
     "0245": BoardInfo(  "IBMEthernetKit",       "k64f",             "l1_k64f.bin"           ),
     "0250": BoardInfo(  "FRDM-KW24D512",        "kw24d5",           "l1_kw24d5.bin"         ),
