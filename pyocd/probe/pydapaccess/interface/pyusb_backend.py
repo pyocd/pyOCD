320b19c51d71ed878f52c16dfe4de9574f99ca0b
Patrick Huesmann <patrick.huesmann@desy.de>
2020-11-03 18:03:54 +0100

    Add copyright for contribution

--- pyocd/probe/pydapaccess/interface/pyusb_backend.py
+++ pyocd/probe/pydapaccess/interface/pyusb_backend.py
@@ -1,5 +1,6 @@
 # pyOCD debugger
 # Copyright (c) 2006-2020 Arm Limited
+# Copyright (c) 2020 Patrick Huesmann
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
