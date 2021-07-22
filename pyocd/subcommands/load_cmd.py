907cd982ec86a7e89e1b0487e44e5cfeea148509
Chris Reed <flit@me.com>
2021-07-22 09:47:43 -0500

    flash subcommand: change into load subcommand.

--- pyocd/subcommands/load_cmd.py
+++ pyocd/subcommands/load_cmd.py
@@ -28,11 +28,12 @@ from ..utility.cmdline import (
 
 LOG = logging.getLogger(__name__)
 
-class FlashSubcommand(SubcommandBase):
-    """! @brief `pyocd flash` subcommand."""
+class LoadSubcommand(SubcommandBase):
+    """! @brief `pyocd load` and `flash` subcommand."""
     
-    NAMES = ['flash']
-    HELP = "Program an image to device flash."
+    NAMES = ['load', 'flash']
+    HELP = "Load one or more images into target device memory."
+    EPILOG = "Supported file formats are: binary, Intel hex, and ELF32."
     DEFAULT_LOG_LEVEL = logging.WARNING
     
     ## @brief Valid erase mode options.
@@ -45,27 +46,32 @@ class FlashSubcommand(SubcommandBase):
     @classmethod
     def get_args(cls) -> List[argparse.ArgumentParser]:
         """! @brief Add this subcommand to the subparsers object."""
-        flash_parser = argparse.ArgumentParser(description=cls.HELP, add_help=False)
+        parser = argparse.ArgumentParser(description=cls.HELP, add_help=False)
 
-        flash_options = flash_parser.add_argument_group("flash options")
-        flash_options.add_argument("-e", "--erase", choices=cls.ERASE_OPTIONS, default='sector',
+        parser_options = parser.add_argument_group("load options")
+        parser_options.add_argument("-e", "--erase", choices=cls.ERASE_OPTIONS, default='sector',
             help="Choose flash erase method. Default is sector.")
-        flash_options.add_argument("-a", "--base-address", metavar="ADDR", type=int_base_0,
-            help="Base address used for the address where to flash a binary. Defaults to start of flash.")
-        flash_options.add_argument("--trust-crc", action="store_true",
+        parser_options.add_argument("-a", "--base-address", metavar="ADDR", type=int_base_0,
+            help="Base address used for the address where to write a binary. Defaults to start of flash. "
+                 "Only allowed if a single binary file is being loaded.")
+        parser_options.add_argument("--trust-crc", action="store_true",
             help="Use only the CRC of each page to determine if it already has the same data.")
-        flash_options.add_argument("--format", choices=("bin", "hex", "elf"),
-            help="File format. Default is to use the file's extension.")
-        flash_options.add_argument("--skip", metavar="BYTES", default=0, type=int_base_0,
-            help="Skip programming the first N bytes. This can only be used with binary files.")
-        flash_options.add_argument("file", metavar="PATH",
-            help="File to program into flash.")
+        parser_options.add_argument("--format", choices=("bin", "hex", "elf"),
+            help="File format. Default is to use the file's extension. If multiple files are provided, then "
+                 "all must be of this type.")
+        parser_options.add_argument("--skip", metavar="BYTES", default=0, type=int_base_0,
+            help="Skip programming the first N bytes. Binary files only.")
+        parser_options.add_argument("file", metavar="<file-path>", nargs="+",
+            help="File to write to memory.")
         
-        return [cls.CommonOptions.COMMON, cls.CommonOptions.CONNECT, flash_parser]
+        return [cls.CommonOptions.COMMON, cls.CommonOptions.CONNECT, parser]
     
     def invoke(self) -> int:
-        """! @brief Handle 'flash' subcommand."""
-        self._increase_logging(["pyocd.flash.loader"])
+        """! @brief Handle 'load' subcommand."""
+        self._increase_logging(["pyocd.flash.loader", __name__])
+        
+        # Validate arguments.
+        
         
         session = ConnectHelper.session_with_chosen_probe(
                             project_dir=self._args.project_dir,
@@ -80,16 +86,18 @@ class FlashSubcommand(SubcommandBase):
                             connect_mode=self._args.connect_mode,
                             options=convert_session_options(self._args.options))
         if session is None:
-            LOG.error("No device available to flash")
+            LOG.error("No target device available")
             return 1
         with session:
             programmer = FileProgrammer(session,
                             chip_erase=self._args.erase,
                             trust_crc=self._args.trust_crc)
-            programmer.program(self._args.file,
-                            base_address=self._args.base_address,
-                            skip=self._args.skip,
-                            file_format=self._args.format)
+            for filename in self._args.file:
+                LOG.info("Loading %s", filename)
+                programmer.program(filename,
+                                base_address=self._args.base_address,
+                                skip=self._args.skip,
+                                file_format=self._args.format)
 
         return 0
 
