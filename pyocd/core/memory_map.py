52f03fe28928e433666884fce1d271e824e14908
Chris Reed <flit@me.com>
2022-08-20 17:35:39 -0500

    memory_map: MemoryRange: add .iter_split_by_address() and .iter_split_by_range() methods with unit tests

--- pyocd/core/memory_map.py
+++ pyocd/core/memory_map.py
@@ -1,6 +1,6 @@
 # pyOCD debugger
 # Copyright (c) 2015-2019 Arm Limited
-# Copyright (c) 2021 Chris Reed
+# Copyright (c) 2021-2022 Chris Reed
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,6 +20,7 @@ import collections.abc
 import copy
 from functools import total_ordering
 from typing import (Any, Dict, Iterable, Iterator, List, Optional, TYPE_CHECKING, Sequence, Tuple, Type, Union)
+from typing_extensions import Self
 
 from ..utility.strings import uniquify_name
 
@@ -36,7 +37,7 @@ class MemoryType(Enum):
     DEVICE = 4
 
 def check_range(
-            start: Union[int, "MemoryRangeBase"],
+            start: Union[int, "MemoryRangeBase", None] = None,
             end: Optional[int] = None,
             length: Optional[int] = None,
             range: Optional["MemoryRangeBase"] = None
@@ -90,7 +91,7 @@ class MemoryRangeBase:
 
     def contains_range(
                 self,
-                start: Union[int, "MemoryRangeBase"],
+                start: Union[int, "MemoryRangeBase", None] = None,
                 end: Optional[int] = None,
                 length: Optional[int] = None,
                 range: Optional["MemoryRangeBase"] = None
@@ -101,7 +102,7 @@ class MemoryRangeBase:
 
     def contained_by_range(
                 self,
-                start: Union[int, "MemoryRangeBase"],
+                start: Union[int, "MemoryRangeBase", None] = None,
                 end: Optional[int] = None,
                 length: Optional[int] = None,
                 range: Optional["MemoryRangeBase"] = None
@@ -112,7 +113,7 @@ class MemoryRangeBase:
 
     def intersects_range(
                 self,
-                start: Union[int, "MemoryRangeBase"],
+                start: Union[int, "MemoryRangeBase", None] = None,
                 end: Optional[int] = None,
                 length: Optional[int] = None,
                 range: Optional["MemoryRangeBase"] = None
@@ -121,6 +122,39 @@ class MemoryRangeBase:
         start, end = check_range(start, end, length, range)
         return (start <= self.start and end >= self.start) or (start <= self.end and end >= self.end) \
             or (start >= self.start and end <= self.end)
+
+    def iter_split_by_address(self, addresses: Iterable[int]) -> Iterator["MemoryRange"]:
+        """@brief Yield ranges by splitting the object at the given addresses.
+
+        The values in _addresses_ are points at which _self_'s bounds are split. Each value in _addresses_
+        which is contained by _self_ becomes the start address of a yielded MemoryRange object, as well as
+        the end + 1 of the previous yielded range.
+
+        If a value in _addresses_ is not contained by _self_, it is ignored. If _addresses_ is empty or
+        contains only out of bounds values, a MemoryRange with the same bounds as _self_ is yielded, unless
+        _self_ has zero length.
+
+        @param self
+        @param addresses Iterable of int addresses by which _self_ is split. Can be in any order, can be,
+            empty and can contain values outside the bounds of _self_.
+        """
+        prev = self.start
+        for a in sorted(a for a in addresses if self.contains_address(a)):
+            if a > prev:
+                yield MemoryRange(start=prev, end=(a - 1))
+            prev = a
+        if prev <= self.end:
+            yield MemoryRange(start=prev, end=self.end)
+
+    def iter_split_by_range(self, other: "MemoryRangeBase") -> Iterator["MemoryRange"]:
+        """@brief Yield a list of ranges before, containing, and after the provided range.
+
+        Up to three MemoryRange objects are yielded. No yielded range will have bounds outside those of
+        self's.
+        - If _self_ and _other_ do not intersect, no ranges are yielded.
+        - If _other_ starts after _self_, a range
+        """
+        yield from self.iter_split_by_address([other.start, other.end + 1])
 
     def __hash__(self) -> int:
         return hash("%08x%08x%08x" % (self.start, self.end, self.length))
@@ -298,7 +332,7 @@ class MemoryRegion(MemoryRangeBase):
         """
         return dict(start=self.start, length=self.length, **self._attributes)
 
-    def clone_with_changes(self, **modified_attrs: Any) -> Any: # Have to return Any because Self isn't available yet.
+    def clone_with_changes(self, **modified_attrs: Any) -> Self:
         """@brief Create a duplicate this region with some of its attributes modified."""
         new_attrs = self._get_attributes_for_clone()
         new_attrs.update(modified_attrs)
