c6b1597c1805e9eaa20da141f1411e88780b3b47
Samuel Dewan <samueldewan@users.noreply.github.com>
2022-10-22 16:05:48 -0400

    rtt: refactor into reusable component and add support to gdbserver (#1443)
    * rtt: refactor rtt implementation
    
    * rtt: add rtt suport to gdbserver

--- pyocd/gdbserver/gdbserver_commands.py
+++ pyocd/gdbserver/gdbserver_commands.py
@@ -19,6 +19,7 @@ import logging
 
 from ..core import exceptions
 from ..commands.base import CommandBase
+from ..utility.rtt_server import RTTServer
 
 LOG = logging.getLogger(__name__)
 
@@ -125,3 +126,127 @@ class GdbserverMonitorExitCommand(CommandBase):
     def execute(self):
         for server in self.context.session.gdbservers.values():
             server.stop(wait=False)
+
+class RTTCommand(CommandBase):
+    INFO = {
+            'names': ['rtt'],
+            'group': 'gdbserver',
+            'category': 'rtt',
+            'nargs': "*",
+            'usage': "rtt {setup,start,stop,channels,server}",
+            'help': "Control SEGGER RTT compatible interface.",
+            }
+
+    def parse(self, args):
+        if len(args) < 1:
+            raise exceptions.CommandError("too few arguments")
+
+        if args[0] == 'setup':
+            if len(args) < 4:
+                raise exceptions.CommandError("too few arguments")
+
+            try:
+                self.addr = int(args[1], 0)
+                self.size = int(args[2], 0)
+                self.id = " ".join(args[3:]).encode("utf-8")
+            except ValueError as e:
+                raise exceptions.CommandError("invalid action") from e
+        elif args[0] == 'start' or args[0] == 'stop' or args[0] == 'channels':
+            if len(args) > 1:
+                raise exceptions.CommandError("too many arguments")
+        elif args[0] == 'server':
+            if len(args) < 2:
+                    raise exceptions.CommandError("too few arguments")
+
+            if args[1] == 'start':
+                if len(args) < 4:
+                    raise exceptions.CommandError("too few arguments")
+                elif len(args) > 4:
+                    raise exceptions.CommandError("too many arguments")
+
+                try:
+                    self.port = int(args[2], 0)
+                    self.channel = int(args[3], 0)
+                except ValueError as e:
+                    raise exceptions.CommandError("invalid action*") from e
+            elif args[1] == 'stop':
+                if len(args) < 3:
+                    raise exceptions.CommandError("too few arguments")
+                elif len(args) > 3:
+                    raise exceptions.CommandError("too many arguments")
+
+                try:
+                    self.port = int(args[2], 0)
+                except ValueError as e:
+                    raise exceptions.CommandError("invalid action") from e
+            else:
+                raise exceptions.CommandError("invalid action")
+
+            self.server_action = args[1]
+        else:
+            raise exceptions.CommandError("invalid action")
+
+        self.action = args[0]
+
+    def execute(self):
+        # Get the gdbserver for the selected core.
+        core_number = self.context.selected_core.core_number
+        try:
+            gdbserver = self.context.session.gdbservers[core_number]
+        except KeyError:
+            raise exceptions.CommandError("no gdbserver for core #%i" % core_number)
+
+        if self.action == "setup":
+            if gdbserver.rtt_server is not None:
+                if gdbserver.rtt_server.running:
+                    raise exceptions.CommandError("rtt is already running")
+                else:
+                    gdbserver.rtt_server = None
+
+            try:
+                gdbserver.rtt_server = RTTServer(gdbserver.target, address = self.addr,
+                                                 size = self.size,
+                                                 control_block_id = self.id)
+            except exceptions.RTTError as e:
+                raise exceptions.CommandError(str(e)) from e
+        elif self.action == "start":
+            if gdbserver.rtt_server is None:
+                raise exceptions.CommandError("rtt is not configured")
+
+            try:
+                gdbserver.rtt_server.start()
+            except exceptions.RTTError as e:
+                raise exceptions.CommandError(str(e)) from e
+
+            self.context.write(f"Found RTT control block.")
+        elif self.action == "stop":
+            if gdbserver.rtt_server is not None:
+                gdbserver.rtt_server.stop()
+        elif self.action == "channels":
+            control_block = gdbserver.rtt_server.control_block
+            self.context.write(f"Channels: up={len(control_block.up_channels)}, "
+                               f"down={len(control_block.down_channels)}")
+            self.context.write("Up-channels:")
+            for i, chan in enumerate(control_block.up_channels):
+                name = chan.name if chan.name is not None else ""
+                self.context.write(f"{i}: {name} {chan.size}")
+            self.context.write("Down-channels:")
+            for i, chan in enumerate(control_block.up_channels):
+                name = chan.name if chan.name is not None else ""
+                self.context.write(f"{i}: {name} {chan.size}")
+        elif self.action == "server":
+            if gdbserver.rtt_server is None:
+                raise exceptions.CommandError("rtt is not configured")
+            elif not gdbserver.rtt_server.running:
+                raise exceptions.CommandError("rtt is not yet started")
+
+            if self.server_action == "start":
+                try:
+                    gdbserver.rtt_server.add_server(self.port, self.channel)
+                except exceptions.RTTError as e:
+                    raise exceptions.CommandError(str(e)) from e
+            elif self.server_action == "stop":
+                try:
+                    gdbserver.rtt_server.stop_server(self.port)
+                except exceptions.RTTError as e:
+                    raise exceptions.CommandError(str(e)) from e
