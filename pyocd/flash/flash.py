e6a6c99fb50a086edccf387ab7e145c575283758
Maximilian Deubel <105227798+maxd-nordic@users.noreply.github.com>
2023-04-13 22:20:47 +0200

    flash: catch exceptions while fetching state (#1519)
    This patch catches (DAPLink) probe timeouts while waiting for a flash
    operation. These timeouts can occur on targets that are stalled for
    a long time. It also reverts my previous attempt to fix the issue,
    which is unnecessary now and might cause other timeouts.

--- pyocd/flash/flash.py
+++ pyocd/flash/flash.py
@@ -1,6 +1,7 @@
 # pyOCD debugger
 # Copyright (c) 2013-2020 Arm Limited
 # Copyright (c) 2021 Chris Reed
+# Copyright (c) 2023 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -622,9 +623,14 @@ class Flash:
         state = Target.State.RUNNING # lgtm[py/multiple-definition]
         with Timeout(timeout) as time_out:
             while time_out.check():
-                state = self.target.get_state()
-                if state != Target.State.RUNNING:
-                    break
+                try:
+                    state = self.target.get_state()
+                    if state != Target.State.RUNNING:
+                        break
+                except exceptions.TransferTimeoutError:
+                    LOG.debug("target.get_state probe timeout")
+                except exceptions.TransferFaultError:
+                    LOG.debug("target.get_state probe fault")
             else:
                 # Operation timed out.
                 self.target.halt()
