a72fa6fa45368d75084e252405cf05c3a62f3d44
Joshua Wong <joshua@northernmechatronics.com>
2023-10-05 22:43:57 -0400

    added logging and updated comment

--- pyocd/target/family/target_ama3b.py
+++ pyocd/target/family/target_ama3b.py
@@ -14,20 +14,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
+
 from ...coresight.cortex_m import CortexM
+
+LOG = logging.getLogger(__name__)
 
 class AMA3BFamily(CortexM):
     REG_MCU_CTRL_BOOTLOADER = 0x400201A0
     REG_MCU_CTRL_SCRATCH0 = 0x400201B0
 
     def set_reset_catch(self, reset_type=None):
-        # Refer to document A-SOCA3B-UGGA02EN for details
         # If Debugger Support is disabled by the SDBG bit in INFO0_SECURITY,
         # The least significant bit of register REG_MCU_CTRL_SCRATCH0 must be
         # set to indicate that a halt is requested by the debugger after
         # primary boot.
+        #
+        # Refer to document A-SOCA3B-UGGA02EN for more details.
         
-        # Check if secure boot is enabled for:
+        # Check the REG_MCU_CTRL_BOOTLOADER register to see if secure boot
+        # is enabled for:
         #   bit 31:30 warm reset
         #   bit 29:28 cold reset
         #   bit 27:26 secure boot feature enabled
@@ -35,6 +41,7 @@ class AMA3BFamily(CortexM):
         reg_bootloader = self.read_memory(self.REG_MCU_CTRL_BOOTLOADER)
         if (reg_bootloader & 0xFC000000):
             secure_boot = True
+        LOG.info("AMA3B Secure Boot: %x" % secure_boot)
         
         if(secure_boot is True):
             # Modify only the least significant bit and preserve the scratch
@@ -42,4 +49,5 @@ class AMA3BFamily(CortexM):
             reg_scratch0 = self.read_memory(self.REG_MCU_CTRL_SCRATCH0) | 0x01
             self.write_memory(self.REG_MCU_CTRL_SCRATCH0, reg_scratch0)
         else:
+            LOG.debug("normal_set_reset_catch")
             super(AMA3BFamily, self).set_reset_catch(reset_type)
