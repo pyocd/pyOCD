647feadfe1a5e61949351b8dc556f2f9b3393032
crypto-lars <77447682+crypto-lars@users.noreply.github.com>
2025-10-17 14:30:56 +0200

    probe: pyusb_v2: fix rx_task setting the wrong *_thread_did_exit flag (#1824)

--- pyocd/probe/pydapaccess/interface/pyusb_v2_backend.py
+++ pyocd/probe/pydapaccess/interface/pyusb_v2_backend.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2019-2021 Arm Limited
 # Copyright (c) 2021 mentha
 # Copyright (c) 2021-2023 Chris Reed
+# Copyright (c) 2025 Lars Häring
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -186,7 +187,7 @@ class PyUSBv2(Interface):
             TRACE.debug("rx_task exception: %s", err)
             self._read_thread_exception = err
         finally:
-            self._swo_thread_did_exit = True
+            self._read_thread_did_exit = True
 
     def swo_rx_task(self):
         try:
